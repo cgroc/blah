0371b8b538ed668be5fcfe7e31164e3945f303cf
Chris Grocott <chris.grocott@gmail.com>
2020-01-08 16:54:27 +0000

    Add avro codec stuff

--- build.sbt
+++ build.sbt
@@ -2,6 +2,12 @@ val CirceVersion = "0.11.1"
 val LogbackVersion = "1.2.3"
 val Fs2Kafka = "0.20.2"
 val ScalaTest = "3.1.0"
+val KafkaSerializationV = "0.5.21"
+
+resolvers in ThisBuild ++= Seq(
+  "Ovo" at "https://dl.bintray.com/ovotech/maven/",
+  "Confluent" at "http://packages.confluent.io/maven/"
+)
 
 lazy val root = (project in file("."))
   .settings(
@@ -10,11 +16,13 @@ lazy val root = (project in file("."))
     version := "0.0.1-SNAPSHOT",
     scalaVersion := "2.12.8",
     libraryDependencies ++= Seq(
-      "com.ovoenergy"   %% "fs2-kafka"           % Fs2Kafka,
-      "io.circe"        %% "circe-generic"       % CirceVersion,
-      "io.circe"        %% "circe-parser"        % CirceVersion,
-      "org.scalatest"   %% "scalatest"           % ScalaTest % "test",
-      "ch.qos.logback"  %  "logback-classic"     % LogbackVersion
+      "com.ovoenergy"   %% "fs2-kafka"                    % Fs2Kafka,
+      "io.circe"        %% "circe-generic"                % CirceVersion,
+      "io.circe"        %% "circe-parser"                 % CirceVersion,
+      "org.scalatest"   %% "scalatest"                    % ScalaTest % "test",
+      "ch.qos.logback"  %  "logback-classic"              % LogbackVersion,
+      "com.ovoenergy"   %% "kafka-serialization-core"     % KafkaSerializationV,
+      "com.ovoenergy"   %% "kafka-serialization-avro4s2"  % KafkaSerializationV
     ),
     addCompilerPlugin("org.typelevel" %% "kind-projector"     % "0.10.3"),
     addCompilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.0")
--- src/main/scala/uk/co/grokemon/blah/Main.scala
+++ src/main/scala/uk/co/grokemon/blah/Main.scala
@@ -2,38 +2,33 @@ package uk.co.grokemon.blah
 
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
-import io.circe._
-import io.circe.generic.semiauto.deriveDecoder
-import io.circe.parser.decode
 import fs2.kafka._
 import fs2.Stream
 import cats.effect.Sync
 import scala.util.control.NoStackTrace
 
+import com.ovoenergy.kafka.serialization.core._
+import com.ovoenergy.kafka.serialization.avro4s2._
+
+import com.sksamuel.avro4s._
+
 object Main extends IOApp {
-  
+
+  val schemaRegistryEndpoint: String = "http://localhost:8081"
+
   final case class MyMessage(sup: String)
-
-  implicit val myMessageDecoder: Decoder[MyMessage] = deriveDecoder
-
-  final case class DecodingException(oopsie: String) extends NoStackTrace
-
-  implicit def myMessageDeserialiser[F[_]: Sync]: Deserializer[F, MyMessage] =
-    Deserializer.string[F].map {
-      decode[MyMessage](_) match {
-        case Right(r) => r
-        case Left(err) => throw new DecodingException(s"$err")
-      }
-    }.handleErrorWith {
-      case oopsie: DecodingException => Deserializer.fail(oopsie)
-    }
 
   def run(args: List[String]): IO[ExitCode] =
     thing.merge(producerStream).compile.drain.as(ExitCode.Success)
   
-  val consumerSettings: ConsumerSettings[IO, Option[String], Option[String]] = 
-    ConsumerSettings[IO, Option[String], Option[String]]
-      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+  implicit val myMessageFromRecord: FromRecord[MyMessage] = FromRecord[MyMessage]
+
+  implicit val valueDeserializer: Deserializer[IO, MyMessage] = 
+    Deserializer.delegate(avroBinarySchemaIdDeserializer[MyMessage](schemaRegistryEndpoint, isKey = false, includesFormatByte = true))
+
+  val consumerSettings: ConsumerSettings[IO, Option[String], MyMessage] = 
+    ConsumerSettings[IO, Option[String], MyMessage]
+      .withAutoOffsetReset(AutoOffsetReset.Latest)
       .withBootstrapServers("localhost:9092")
       .withGroupId("group")
 
@@ -41,20 +36,25 @@ object Main extends IOApp {
     consumerStream[IO].using(consumerSettings)
       .evalTap(_.subscribeTo("test"))
       .flatMap(_.stream)
-      .map { c => //println(c)
+      .map { c =>
         val r = c.record
-        println(s"Received message! === ${r.value.getOrElse("")} ===")
+        println(s"Received message! === ${r.value} ===")
       }
 
-  val producerSettings: ProducerSettings[IO, Option[String], Option[String]] =
-    ProducerSettings[IO, Option[String], Option[String]]
+  implicit val myMessageToRecord: ToRecord[MyMessage] = ToRecord[MyMessage]
+
+  implicit val valueSerializer: Serializer[IO, MyMessage] = 
+    Serializer.delegate(avroBinarySchemaIdSerializer[MyMessage](schemaRegistryEndpoint, isKey = false, includesFormatByte = true))
+
+  val producerSettings: ProducerSettings[IO, Option[String], MyMessage] =
+    ProducerSettings[IO, Option[String], MyMessage]
       .withBootstrapServers("localhost:9092")
 
   def producerStream = Stream.repeatEval {
     IO {
       println("Enter message:\n")
       val message = scala.io.StdIn.readLine()
-      ProducerRecords.one(ProducerRecord("test", None, Some(message)))
+      ProducerRecords.one(ProducerRecord("test", None, MyMessage(message)))
     }
   }.through(produce(producerSettings))
   
